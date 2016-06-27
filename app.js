11621af77b18e9fc007dde2663b93b2251729bfb
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-27 10:57:57 -0500

    JavaScript Comments

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myp"
+in my web page:
+*/
+document.getElementById("myH").innerHTML = "My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
+
+var x = 5;
+var y = x + 2;
+
+document.getElementById("demo").innerHTML = y; // Write y to demo
